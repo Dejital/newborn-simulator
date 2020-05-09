aeea7a83763a5ab8b860a42fbf581dc88b82a484
Serge Nevsky <serge@snevsky.com>
2020-05-08 23:19:21 -0500

    Updates

---
+++ precache-manifest.bdb253ae9a2d8b3f1290ff859cf12159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c08e72a7eb96e97cfac875d12e7d0a",
+    "url": "/newborn-simulator/index.html"
+  },
+  {
+    "revision": "b9356d1074ada1e09d2a",
+    "url": "/newborn-simulator/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "e49f81d81fc1cdc38244",
+    "url": "/newborn-simulator/static/js/2.fc317384.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/newborn-simulator/static/js/2.fc317384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9356d1074ada1e09d2a",
+    "url": "/newborn-simulator/static/js/main.4f520cbb.chunk.js"
+  },
+  {
+    "revision": "746d27a611d50cb26d57",
+    "url": "/newborn-simulator/static/js/runtime-main.89cb5365.js"
+  }
+]);
